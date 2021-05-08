1a3cfba5f59fc8d8f7229bc26b0b30e52db1550c
manav014 <dpsman13016@gmail.com>
2021-05-08 20:46:58 +0530

    Folders for CheckoutDelivery

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Redirect } from "react-router-dom";
 import HomePage from "./components/HomePage.js";
+import Address from "./components/CheckoutAddress.js";
 
 function App() {
   return (
     <div>
       <Router>
         <Switch>
+          <Route exact path="/address" component={Address} />
           <Route exact path="/" component={HomePage} />
           <Redirect from="*" to="/" />
         </Switch>
