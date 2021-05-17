6b30e249953a33eceb50778c37517d4008e2f570
avanya080 <avanya.wadhwa@gmail.com>
2021-05-17 21:27:07 +0530

    minor

--- src/Components/Checkout/AddressCard.js
+++ src/Components/Checkout/AddressCard.js
@@ -1,10 +1,9 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
-import CardActionArea from "@material-ui/core/CardActionArea";
 import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
-import CardMedia from "@material-ui/core/CardMedia";
+import CardHeader from "@material-ui/core/CardHeader";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 import PropTypes from "prop-types";
