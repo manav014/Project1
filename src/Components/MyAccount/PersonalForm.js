20a16a4621ba13879d8c45354cc0079446237361
khu5h1 <60094678+khu5h1@users.noreply.github.com>
2021-06-02 04:42:27 +0530

    Update PersonalForm.js

--- src/Components/MyAccount/PersonalForm.js
+++ src/Components/MyAccount/PersonalForm.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 // @material-ui components
 import TextField from "@material-ui/core/TextField";
@@ -11,6 +11,7 @@ import AppBar from "@material-ui/core/AppBar";
 import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
 import Typography from "@material-ui/core/Typography";
+import MenuItem from "@material-ui/core/MenuItem";
 
 const useStyles = makeStyles((theme) => ({
   buttons: {
@@ -38,13 +39,56 @@ function TabPanel(props) {
   );
 }
 
-function PersonalForm() {
+function PersonalForm(props) {
+  const gender = [
+    {
+      abbreviation: "M",
+      name: "Male",
+    },
+    {
+      abbreviation: "F",
+      name: "Female",
+    },
+    {
+      abbreviation: "Others",
+      name: "Others",
+    },
+  ];
   const classes = useStyles();
   const theme = useTheme();
   const [value, setValue] = React.useState(0);
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
+  };
+  const [formData, setFormData] = useState({
+    firstname: "",
+    lastname: "",
+    email: "",
+    contact: "",
+    dob: "",
+    gender: "",
+  });
+  const onChangeHandler = (e) => {
+    console.log(e.target.name);
+    console.log(e.target.value);
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+  const mobileOnChangeHandler = (e) => {
+    if (e.target.value.length >= 11 || isNaN(e.target.value.slice(-1))) {
+      setFormData({
+        ...formData,
+        [e.target.name]: formData.contact,
+      });
+    } else {
+      setFormData({
+        ...formData,
+        [e.target.name]: e.target.value,
+      });
+    }
   };
   return (
     <ThemeProvider theme={theme}>
@@ -61,6 +105,7 @@ function PersonalForm() {
               <TextField
                 required
                 id="firstName"
+                onChange={onChangeHandler}
                 name="firstName"
                 label="First name"
                 fullWidth
@@ -73,6 +118,7 @@ function PersonalForm() {
                 id="lastName"
                 name="lastName"
                 label="Last name"
+                onChange={onChangeHandler}
                 fullWidth
                 autoComplete="family-name"
               />
@@ -83,18 +129,28 @@ function PersonalForm() {
                 id="email"
                 name="email"
                 label="Email"
+                inputProps={{
+                  pattern: "[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,}$",
+                }}
                 fullWidth
                 autoComplete="Email"
+                onChange={onChangeHandler}
               />
             </Grid>
             <Grid item xs={12} sm={6}>
               <TextField
+                inputProps={{
+                  pattern: "[1-9]{1}[0-9]{9}",
+                }}
+                label
                 required
                 id="contact"
                 name="contact"
                 label="Contact"
                 fullWidth
+                value={formData.contact}
                 autoComplete="contact"
+                onChange={mobileOnChangeHandler}
               />
             </Grid>
             <Grid item xs={12} sm={6}>
@@ -105,17 +161,26 @@ function PersonalForm() {
                 label="DOB"
                 fullWidth
                 autoComplete="dob"
+                onChange={onChangeHandler}
               />
             </Grid>
             <Grid item xs={12} sm={6}>
               <TextField
+                select
                 required
                 id="gender"
                 name="gender"
                 label="Gender"
                 fullWidth
-                autoComplete="gender"
-              />
+                onChange={onChangeHandler}
+                value={formData.gender}
+              >
+                {gender.map((option, key) => (
+                  <MenuItem key={key} value={option.name} name={option.name}>
+                    {option.name}
+                  </MenuItem>
+                ))}
+              </TextField>
             </Grid>
             <Grid item xs={12}>
               <div className={classes.buttons}>
