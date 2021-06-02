94bd3c68c94a32bfc402195697cfa78df99c2b30
khu5h1 <khushirauniyar8@gmail.com>
2021-06-03 01:11:36 +0530

    Imports Minors

--- src/Components/MyAccount/SideBar.js
+++ src/Components/MyAccount/SideBar.js
@@ -14,6 +14,11 @@ import Divider from "@material-ui/core/Divider";
 import Paper from "@material-ui/core/Paper";
 import Typography from "@material-ui/core/Typography";
 import Avatar from "@material-ui/core/Avatar";
+import { ThemeProvider } from "@material-ui/core/styles";
+
+// local components
+import theme from "../../consts/theme";
+
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -31,112 +36,121 @@ const useStyles = makeStyles((theme) => ({
 function SideBar() {
   const classes = useStyles();
   return (
-    <div>
-      <List className={classes.root}>
-        <Paper variant="outlined" style={{ padding: "1vh", marginTop: "2vh" }}>
-          <ListItem>
-            <ListItemIcon>
-              <Avatar className={classes.avatar}>
-                <AccountBoxIcon />
-              </Avatar>
-            </ListItemIcon>
-            <ListItemText
-              primary={<Typography variant="button">My Account</Typography>}
-            />
-          </ListItem>
-          <List component="div" disablePadding>
-            <ListItem button className={classes.nested}>
+    <ThemeProvider theme={theme}>
+      <div>
+        <List className={classes.root}>
+          <Paper
+            variant="outlined"
+            style={{ padding: "1vh", marginTop: "2vh" }}
+          >
+            <ListItem>
+              <ListItemIcon>
+                <Avatar className={classes.avatar}>
+                  <AccountBoxIcon />
+                </Avatar>
+              </ListItemIcon>
               <ListItemText
-                primary={
-                  <Typography variant="subtitle1">
-                    Edit Personal Information
-                  </Typography>
-                }
+                primary={<Typography variant="button">My Account</Typography>}
               />
             </ListItem>
-            <ListItem button className={classes.nested}>
+            <List component="div" disablePadding>
+              <ListItem button className={classes.nested}>
+                <ListItemText
+                  primary={
+                    <Typography variant="subtitle1">
+                      Edit Personal Information
+                    </Typography>
+                  }
+                />
+              </ListItem>
+              <ListItem button className={classes.nested}>
+                <ListItemText
+                  primary={
+                    <Typography variant="subtitle1">Change Password</Typography>
+                  }
+                />
+              </ListItem>
+            </List>
+            <Divider />
+            <ListItem>
+              <ListItemIcon>
+                <Avatar className={classes.avatar}>
+                  <PaymentIcon />
+                </Avatar>
+              </ListItemIcon>
               <ListItemText
-                primary={
-                  <Typography variant="subtitle1">Change Password</Typography>
-                }
+                primary={<Typography variant="button"> Payment</Typography>}
               />
             </ListItem>
-          </List>
-          <Divider />
-          <ListItem>
-            <ListItemIcon>
-              <Avatar className={classes.avatar}>
-                <PaymentIcon />
-              </Avatar>
-            </ListItemIcon>
-            <ListItemText
-              primary={<Typography variant="button"> Payment</Typography>}
-            />
-          </ListItem>
-          <List component="div" disablePadding>
-            <ListItem button className={classes.nested}>
+            <List component="div" disablePadding>
+              <ListItem button className={classes.nested}>
+                <ListItemText
+                  primary={
+                    <Typography variant="subtitle1">Manage Cards</Typography>
+                  }
+                />
+              </ListItem>
+            </List>
+            <Divider />
+            <ListItem>
+              <ListItemIcon>
+                <Avatar className={classes.avatar}>
+                  <HomeIcon />
+                </Avatar>
+              </ListItemIcon>
               <ListItemText
-                primary={
-                  <Typography variant="subtitle1">Manage Cards</Typography>
-                }
+                primary={<Typography variant="button">Address</Typography>}
               />
             </ListItem>
-          </List>
-          <Divider />
-          <ListItem>
-            <ListItemIcon>
-              <Avatar className={classes.avatar}>
-                <HomeIcon />
-              </Avatar>
-            </ListItemIcon>
-            <ListItemText
-              primary={<Typography variant="button">Address</Typography>}
-            />
-          </ListItem>
-          <List component="div" disablePadding>
-            <ListItem button className={classes.nested}>
+            <List component="div" disablePadding>
+              <ListItem button className={classes.nested}>
+                <ListItemText
+                  primary={
+                    <Typography variant="subtitle1">
+                      Manage Addresses
+                    </Typography>
+                  }
+                />
+              </ListItem>
+            </List>
+            <Divider />
+            <ListItem>
+              <ListItemIcon>
+                <Avatar className={classes.avatar}>
+                  <FileCopyIcon />
+                </Avatar>
+              </ListItemIcon>
               <ListItemText
-                primary={
-                  <Typography variant="subtitle1">Manage Addresses</Typography>
-                }
+                primary={<Typography variant="button"> My Stuff</Typography>}
               />
             </ListItem>
-          </List>
-          <Divider />
-          <ListItem>
-            <ListItemIcon>
-              <Avatar className={classes.avatar}>
-                <FileCopyIcon />
-              </Avatar>
-            </ListItemIcon>
-            <ListItemText
-              primary={<Typography variant="button"> My Stuff</Typography>}
-            />
-          </ListItem>
-          <List component="div" disablePadding>
-            <ListItem button className={classes.nested}>
-              <ListItemText
-                primary={
-                  <Typography variant="subtitle1">Review and Rating</Typography>
-                }
-              />
-            </ListItem>
-            <ListItem button className={classes.nested}>
-              <ListItemText
-                primary={
-                  <Typography variant="subtitle1">My WishList</Typography>
-                }
-              />
-            </ListItem>
-            <ListItem button className={classes.nested}>
-              <ListItemText
-                primary={<Typography variant="subtitle1">Coupons</Typography>}
-              />
-            </ListItem>
-          </List>
-        </Paper>
-      </List>
-    </div>
+            <List component="div" disablePadding>
+              <ListItem button className={classes.nested}>
+                <ListItemText
+                  primary={
+                    <Typography variant="subtitle1">
+                      Review and Rating
+                    </Typography>
+                  }
+                />
+              </ListItem>
+              <ListItem button className={classes.nested}>
+                <ListItemText
+                  primary={
+                    <Typography variant="subtitle1">My WishList</Typography>
+                  }
+                />
+              </ListItem>
+              <ListItem button className={classes.nested}>
+                <ListItemText
+                  primary={<Typography variant="subtitle1">Coupons</Typography>}
+                />
+              </ListItem>
+            </List>
+          </Paper>
+        </List>
+      </div>
+    </ThemeProvider>
   );
 }
 
