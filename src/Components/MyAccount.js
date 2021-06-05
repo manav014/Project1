bd6d7484a4d1868a52e9e3aacb57c4fb38524ccc
khu5h1 <khushirauniyar8@gmail.com>
2021-06-06 00:10:33 +0530

    My accounts page using mobile view

--- src/Components/MyAccount.js
+++ src/Components/MyAccount.js
@@ -10,12 +10,14 @@ import CardHeader from "@material-ui/core/CardHeader";
 import Grid from "@material-ui/core/Grid";
 import Box from "@material-ui/core/Box";
 import Avatar from "@material-ui/core/Avatar";
+import Hidden from "@material-ui/core/Hidden";
 
 // local components
 import HeaderBar from "./HeaderBar.js";
 import LinkedCards from "./MyAccount/LinkedCards";
 import MainContainer from "./MyAccount/MainContainer";
 import SideBar from "./MyAccount/SideBar";
+import SideBarMobile from "./MyAccount/SideBarMobile.js";
 import theme from "../consts/theme";
 
 const useStyles = makeStyles(() => ({
@@ -34,38 +36,44 @@ function MyAccount(props) {
     <ThemeProvider theme={theme}>
       <div>
         <HeaderBar sticky />
-        <Grid
-          container
-          direction="row"
-          justify="space-evenly"
-          alignItems="center"
-        >
-          <Grid item xs={2}>
-            <Box>
-              <CardHeader
-                avatar={
-                  <Avatar className={classes.avatar}>{username[0]}</Avatar>
-                }
-                title="Welcome"
-                subheaderTypographyProps={{ variant: "subtitle2" }}
-                titleTypographyProps={{ variant: "body" }}
-                subheader={username}
-              />
-            </Box>
+        <Hidden smDown>
+          <Grid
+            container
+            direction="row"
+            justify="space-evenly"
+            alignItems="center"
+          >
+            <Grid item xs={2}>
+              <Box>
+                <CardHeader
+                  avatar={
+                    <Avatar className={classes.avatar}>{username[0]}</Avatar>
+                  }
+                  title="Welcome"
+                  subheaderTypographyProps={{ variant: "subtitle2" }}
+                  titleTypographyProps={{ variant: "body" }}
+                  subheader={username}
+                />
+              </Box>
+            </Grid>
+            <Grid item xs={9}>
+              <LinkedCards />
+            </Grid>
           </Grid>
-          <Grid item xs={9}>
-            <LinkedCards />
+          <Grid container direction="row" justify="space-evenly">
+            <Grid item xs={3}>
+              <SideBar />
+            </Grid>
+            <Grid item xs={8}>
+              <MainContainer />
+            </Grid>
           </Grid>
-        </Grid>
-        <Grid container direction="row" justify="space-evenly">
-          <Grid item xs={3}>
-            <SideBar />
-          </Grid>
-          <Grid item xs={8}>
-            <MainContainer />
-          </Grid>
-        </Grid>
+        </Hidden>
       </div>
+
+      <Hidden mdUp>
+        <SideBarMobile />
+      </Hidden>
     </ThemeProvider>
   );
 }
