bb7123bab5d69682c8bafcaa224ae2a25b4b6d36
khu5h1 <khushirauniyar8@gmail.com>
2021-06-07 00:57:10 +0530

    Completed implementation of shopwise order mobile view

--- src/Components/Orders/ShopOrderMobile.js
+++ src/Components/Orders/ShopOrderMobile.js
@@ -1,28 +1,45 @@
 import React from "react";
-import { ThemeProvider, makeStyles } from "@material-ui/core/styles";
+import {
+  ThemeProvider,
+  makeStyles,
+  withStyles,
+} from "@material-ui/core/styles";
 import theme from "../../consts/theme";
 import Grid from "@material-ui/core/Grid";
-import Paper from "@material-ui/core/Paper";
 import Typography from "@material-ui/core/Typography";
 import ButtonBase from "@material-ui/core/ButtonBase";
 import product1 from "../../assets/HomePage/product1.png";
+import dairy from "../../assets/HomePage/dairy.jpg";
+import Box from "@material-ui/core/Box";
+import Accordion from "@material-ui/core/Accordion";
+import MuiAccordionSummary from "@material-ui/core/AccordionSummary";
+import AccordionDetails from "@material-ui/core/AccordionDetails";
+import AddBoxIcon from "@material-ui/icons/Add";
+
+const AccordionSummary = withStyles({
+  root: {
+    backgroundColor: "rgba(0, 0, 0, .09)",
+    marginBottom: -1,
+    "&$expanded": {
+      minHeight: 52,
+    },
+  },
+  content: {
+    "&$expanded": {
+      margin: "12px 0",
+    },
+  },
+  expanded: {},
+})(MuiAccordionSummary);
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  paper: {
-    padding: theme.spacing(2),
-    margin: "auto",
-    maxWidth: 500,
-  },
   image: {
-    width: 128,
-    height: 128,
+    width: 100,
+    height: 100,
   },
   img: {
-    margin: "auto",
-    display: "block",
+    // margin: "auto",
+    // display: "block",
     maxWidth: "100%",
     maxHeight: "100%",
   },
@@ -33,92 +50,121 @@ function ShopOrderMobile() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Grid
-        container
-        direction="row"
-        justify="space-evenly"
-        alignItems="center"
-      >
-        <Grid item xs={11}>
-          <Paper
-            variant="outlined"
-            style={{ padding: "1vh", marginTop: "2vh" }}
+      <Grid container direction="row" justify="center" alignItems="center">
+        <Grid item xs={12}>
+          <Box bgcolor="grey.300" display="flex" justifyContent="center">
+            <Typography variant="subtitle1">
+              <b>Order No: 2223</b>
+            </Typography>
+          </Box>
+          <Box m={1} p={1}>
+            <Typography variant="body2">
+              <b>Shipped To:</b> Khushi Rauniyar
+            </Typography>
+            <Typography variant="body2">
+              <b> Store Name:</b> Khushi Store
+            </Typography>
+            <Typography variant="body2">
+              <b>Number Of Items:</b> 4
+            </Typography>
+            <Typography variant="body2">
+              <b>Gross Amount:</b> &#8377; 1345/-
+            </Typography>
+            <Typography variant="body2">
+              <b>Discount:</b> &#8377; 400/-
+            </Typography>
+          </Box>
+          <Box
+            display="flex"
+            bgcolor="grey.300"
+            justifyContent="flex-end"
+            m={1}
           >
-            <Grid item xs>
-              <Typography
-                gutterBottom
-                variant="subtitle1"
-                style={{ fontWeight: "bold" }}
-              >
-                Order No: 2223
-              </Typography>
-              <Typography
-                gutterBottom
-                variant="body2"
-                style={{ fontWeight: "bold" }}
-              >
-                Shipped To:
-              </Typography>
-              <Typography gutterBottom variant="body2">
-                Khushi Rauniyar
-              </Typography>
-              <div style={{ display: "flex" }}>
-                <Typography
-                  variant="body2"
-                  gutterBottom
-                  style={{ fontWeight: "bold" }}
-                >
-                  Store Name:
-                </Typography>
-                <Typography variant="body2" color="textSecondary">
-                  Khushi Stores
-                </Typography>
-              </div>
-            </Grid>
-            <Grid item>
-              <Typography
-                variant="body2"
-                style={{ cursor: "pointer", color: "#7D808E" }}
-              >
-                Remove | Save For Later | See More Like This
-              </Typography>
-            </Grid>
-          </Paper>
+            <Typography variant="subtitle1">
+              Amount:<b> &#8377; 945/-</b>
+            </Typography>
+          </Box>
         </Grid>
-        <Grid item xs={11}>
-          <div className={classes.root}>
-            <Paper className={classes.paper}>
+        <Grid item xs={12}>
+          <div>
+            <Box border={1} m={1} p={2}>
               <Grid container spacing={2}>
-                <Grid item >
+                <Grid item xs>
                   <ButtonBase className={classes.image}>
                     <img className={classes.img} src={product1} />
                   </ButtonBase>
                 </Grid>
-                <Grid item xs={6} sm container>
-                  <Grid item xs container direction="column" spacing={2}>
-                    <Grid item xs>
-                      <Typography gutterBottom variant="h6">
-                        Standard license
-                      </Typography>
-                      <Typography variant="body2" gutterBottom>
-                        Full resolution 1920x1080 • JPEG
-                      </Typography>
-                      <Typography variant="body2" color="textSecondary">
-                        ID: 1030114
-                      </Typography>
-                    </Grid>
-                    <Grid item>
-                      <Typography variant="body2" style={{ cursor: "pointer" }}>
-                        Remove
-                      </Typography>
-                    </Grid>
-                  </Grid>
-                  <Grid item>
-                    <Typography variant="subtitle1">$19.00</Typography>
-                  </Grid>
+                <Grid item xs={7}>
+                  <Typography variant="h6">
+                    <b>Amul 100gm Cheese slices</b>
+                  </Typography>
+                  <Typography variant="body2">
+                    <b>Quantity:</b> 2
+                  </Typography>
+                  <Typography variant="body2">
+                    <b>Price:</b> &#8377; 400/-
+                  </Typography>
+                </Grid>
+                <Grid item xs={12}>
+                  <Accordion>
+                    <AccordionSummary expandIcon={<AddBoxIcon />}>
+                      More Details
+                    </AccordionSummary>
+                    <AccordionDetails>
+                      <Box m={1} p={1}>
+                        <Typography variant="body2">
+                          <b>MRP:</b> &#8377; 600/-
+                        </Typography>
+                        <Typography variant="body2">
+                          <b> Our Price:</b> &#8377; 400/-
+                        </Typography>
+                      </Box>
+                    </AccordionDetails>
+                  </Accordion>
                 </Grid>
               </Grid>
-            </Paper>
+            </Box>
+          </div>
+        </Grid>
+        <Grid item xs={12}>
+          <div>
+            <Box border={1} m={1} p={2}>
+              <Grid container spacing={2}>
+                <Grid item xs>
+                  <ButtonBase className={classes.image}>
+                    <img className={classes.img} src={dairy} />
+                  </ButtonBase>
+                </Grid>
+                <Grid item xs={7}>
+                  <Typography variant="h6">
+                    <b>Amul 100gm Cheese slices</b>
+                  </Typography>
+                  <Typography variant="body2">
+                    <b>Quantity:</b> 2
+                  </Typography>
+                  <Typography variant="body2">
+                    <b>Price:</b> &#8377; 400/-
+                  </Typography>
+                </Grid>
+                <Grid item xs={12}>
+                  <Accordion>
+                    <AccordionSummary expandIcon={<AddBoxIcon />}>
+                      More Details
+                    </AccordionSummary>
+                    <AccordionDetails>
+                      <Box m={1} p={1}>
+                        <Typography variant="body2">
+                          <b>MRP:</b> &#8377; 600/-
+                        </Typography>
+                        <Typography variant="body2">
+                          <b> Our Price:</b> &#8377; 400/-
+                        </Typography>
+                      </Box>
+                    </AccordionDetails>
+                  </Accordion>
+                </Grid>
+              </Grid>
+            </Box>
           </div>
         </Grid>
       </Grid>
